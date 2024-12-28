c421a32bb5505ab022c0a80862fc5e3a78e447e2
hoquocbao23 <hoquocbao6123@gmail.com>
2024-12-28 09:17:42 +0700

    fix install

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     resolve(),
     commonjs(),
-    terser(),],
-})
+    terser(),
+  ],
+});
